41477361ff8aa41576f694f1cb5d2fd6b37ba7f5
mcmancini <m.c.mancini@exeter.ac.uk>
2024-08-12 11:42:07 +0100

    Updated processing routines to work with the new API and cleaned up/refactored the code

--- era_weather/rechunk_weather.py
+++ era_weather/rechunk_weather.py
@@ -11,7 +11,7 @@ import pandas as pd
 import xarray as xr
 from pyproj import Transformer
 
-from era_weather.utils import relative_humidity
+from era_weather.utils import list_files, list_years, relative_humidity
 
 
 def process_weather_cell(file, coords_lonlat):
@@ -22,16 +22,25 @@ def process_weather_cell(file, coords_lonlat):
         latitude=coords_lonlat[1],
         method="nearest",
     )
-    cell_df = cell_ds.to_dataframe()
+    cell_df = cell_ds.to_dataframe().reset_index().dropna()
     cell_df["wspeed"] = np.sqrt(cell_df["u10"] ** 2 + cell_df["v10"] ** 2)
     cell_df["tas"] = cell_df["t2m"] - 273.15
     cell_df["dp"] = cell_df["d2m"] - 273.15
     cell_df["hurs"] = relative_humidity(
         temperature=cell_df["tas"], dewpoint=cell_df["dp"]
     )
-    cell_df["tp"] = cell_df["tp"] * 1000 # rain in m!
+    cell_df["tp"] = cell_df["tp"] * 1000  # rain in m!
 
-    cell_daily = cell_df.groupby(cell_df.index.date).agg(
+    if "time" in cell_df.columns:
+        cell_df.drop(columns=["time"], inplace=True)
+
+    if 'date' not in cell_df.columns:
+        if 'valid_time' in cell_df.columns:
+            cell_df.rename(columns={'valid_time': 'date'}, inplace=True)
+        else:
+            raise ValueError("Neither 'date' nor 'valid_time' column found in the DataFrame.")
+
+    cell_daily = cell_df.groupby(cell_df.date).agg(
         tasmean=("tas", "mean"),
         tasmin=("tas", "min"),
         tasmax=("tas", "max"),
@@ -43,6 +52,41 @@ def process_weather_cell(file, coords_lonlat):
     cell_daily["irrad"] = cell_daily["ssrd"] / (24 * 3600)
     yr_ds.close()
     return cell_daily
+
+
+def aggregate_years(raw_data_folder, start_year, end_year):
+    """Routine to list raw monthly data and aggregate to yearly"""
+    years_to_process = list(range(start_year, end_year + 1))
+    unique_years = list_years(raw_data_folder)
+    yearly_file_list = []
+    for year in set(unique_years).intersection(years_to_process):
+        # Has the data been already aggregated to yearly?
+        yearly_file = list_files(directory=raw_data_folder, year=year, monthly=False)
+        if not yearly_file:
+            monthly_files = list_files(
+                directory=raw_data_folder, year=year, monthly=True
+            )
+            if not monthly_files:
+                raise ValueError(f"No available data for year '{year}'")
+            if len(monthly_files) < 12:
+                raise ValueError(
+                    f"Missing {12 - len(monthly_files)} months of data for year '{year}'!"
+                )
+            datasets = [xr.open_dataset(file) for file in monthly_files]
+            combined_dataset = xr.concat(datasets, dim="valid_time")
+            encoding = {
+                var: {"dtype": combined_dataset[var].dtype}
+                for var in combined_dataset.data_vars
+            }
+            combined_dataset.to_netcdf(
+                f"{raw_data_folder}/era5_surface_ukeire_{year}.nc", encoding=encoding
+            )
+            yearly_file = list_files(
+                directory=raw_data_folder, year=year, monthly=False
+            )
+            yearly_file_list.append(yearly_file)
+        yearly_file_list.append(yearly_file)
+    return yearly_file_list
 
 
 def rechunk_data(row, yearly_file_list, output_path):
@@ -58,5 +102,5 @@ def rechunk_data(row, yearly_file_list, output_path):
         output = pd.concat([output, weather_time_series])
     cell_filename = f"{output_path}{cell_name}.csv"
     output.reset_index(inplace=True)
-    output.rename(columns={'index': 'date'}, inplace=True)
+    output.rename(columns={"index": "date"}, inplace=True)
     output.to_csv(cell_filename, index=False)
